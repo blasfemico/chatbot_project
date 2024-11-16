d2773c73ea2a6e795af61f2b69d01640aa1bd5c3
dante <dvillenablanck@gmail.com>
2024-11-15 23:06:59 -0300

    arreglo de bugs

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from app.database import Base
+from datetime import datetime
+
 
 class FAQ(Base):
     __tablename__ = "faqs"
@@ -8,64 +10,77 @@ class FAQ(Base):
     id = Column(Integer, primary_key=True, index=True)
     question = Column(String, nullable=False)
     answer = Column(String, nullable=False)
-    
+
 class Order(Base):
     __tablename__ = "orders"
+
     id = Column(Integer, primary_key=True, index=True)
     phone = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    address = Column(String, nullable=False)
-class CiudadProducto(Base):
-    __tablename__ = 'ciudad_producto'
-    id = Column(Integer, primary_key=True)
-    ciudad_id = Column(Integer, ForeignKey('ciudades.id'))
-    producto_id = Column(Integer, ForeignKey('productos.id'))
-    
+    email = Column(String, default="N/A")
+    address = Column(String, default="N/A")
+    producto = Column(String, nullable=False)
+    cantidad_cajas = Column(Integer, default=1)
+    ad_id = Column(String, nullable=True)
+    nombre = Column(String, default="N/A")  # Nuevo campo
+    apellido = Column(String, default="N/A")  # Nuevo campo
 
 class Ciudad(Base):
-    __tablename__ = 'ciudades'
+    __tablename__ = "ciudades"
     id = Column(Integer, primary_key=True)
     nombre = Column(String, unique=True, nullable=False)
-    productos = relationship("Producto", secondary="ciudad_producto", back_populates="ciudades")
-    cuentas = relationship("CuentaCiudad", back_populates="ciudad")  
+
+    productos = relationship("ProductoCiudad", back_populates="ciudad")
+
+
+class ProductoCiudad(Base):
+    __tablename__ = "producto_ciudad"
+    id = Column(Integer, primary_key=True)
+    ciudad_id = Column(
+        Integer, ForeignKey("ciudades.id", ondelete="CASCADE"), nullable=False
+    )
+    producto_id = Column(
+        Integer, ForeignKey("productos.id", ondelete="CASCADE"), nullable=False
+    )
+
+    ciudad = relationship("Ciudad", back_populates="productos")
+    producto = relationship("Producto", back_populates="ciudades")
+
 
 class Producto(Base):
-    __tablename__ = 'productos'
+    __tablename__ = "productos"
     id = Column(Integer, primary_key=True)
-    nombre = Column(String, unique=True, nullable=False)
-    ciudades = relationship("Ciudad", secondary="ciudad_producto", back_populates="productos")
+    nombre = Column(String, nullable=False)
+
+    ciudades = relationship("ProductoCiudad", back_populates="producto")
+
 
 class Cuenta(Base):
-    __tablename__ = 'cuentas'
+    __tablename__ = "cuentas"
     id = Column(Integer, primary_key=True)
     nombre = Column(String, unique=True, nullable=False)
-    page_id = Column(String, unique=True, nullable=False)  
+    page_id = Column(String, unique=True, nullable=False)
+    aplicacion_id = Column(Integer, ForeignKey("aplicaciones_facebook.id"))
+    aplicacion = relationship("AplicacionFacebook", back_populates="cuentas")
     productos = relationship("CuentaProducto", back_populates="cuenta")
-    ciudades = relationship("CuentaCiudad", back_populates="cuenta")
 
 class CuentaProducto(Base):
-    __tablename__ = 'cuenta_producto'
+    __tablename__ = "cuenta_producto"
     id = Column(Integer, primary_key=True)
-    cuenta_id = Column(Integer, ForeignKey('cuentas.id'))
-    producto_id = Column(Integer, ForeignKey('productos.id'))
+    cuenta_id = Column(Integer, ForeignKey("cuentas.id"))
+    producto_id = Column(Integer, ForeignKey("productos.id"))
     precio = Column(Float, nullable=False)
     cuenta = relationship("Cuenta", back_populates="productos")
     producto = relationship("Producto")
 
-class CuentaCiudad(Base):
-    __tablename__ = "cuenta_ciudad"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    cuenta_id = Column(Integer, ForeignKey("cuentas.id"))
-    ciudad_id = Column(Integer, ForeignKey("ciudades.id"))
-
-    cuenta = relationship("Cuenta", back_populates="ciudades")
-    ciudad = relationship("Ciudad", back_populates="cuentas")
-
-
 
 class LastProcessedMessage(Base):
-    __tablename__ = 'last_processed_messages'
+    __tablename__ = "last_processed_messages"
+
     id = Column(Integer, primary_key=True, index=True)
-    sender_id = Column(String, unique=True, nullable=False)  
-    last_message_id = Column(String, nullable=False)   
+    sender_id = Column(String, index=True, nullable=False)
+    last_message_id = Column(String, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    message_text = Column(String)
+    intent = Column(String, default=None) 
+    producto = Column(String, nullable=True) 
+
